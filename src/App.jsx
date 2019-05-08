7d7026400087a843a8bd2fe0998d4a5f0ffd9c80
Adriana <ajimenez0024@gmail.com>
2019-05-08 23:50:43 +0000

    passing messages to server

--- src/App.jsx
+++ src/App.jsx
@@ -2,93 +2,47 @@ import React, { Component } from "react";
 import Chatbar from "./ChatBar.jsx";
 import MessageList from "./MessageList.jsx";
 
-const generateRandomId = (alphabet => {
-  const alphabetLength = alphabet.length;
-  const randoIter = (key, n) => {
-    if (n === 0) {
-      return key;
-    }
-    const randoIndex = Math.floor(Math.random() * alphabetLength);
-    const randoLetter = alphabet[randoIndex];
-    return randoIter(key + randoLetter, n - 1);
-  };
-  return () => randoIter("", 10);
-})("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+const URL = "ws://localhost:3001";
+
+function generateRandomString() {
+  var text = "";
+  var possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 7; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       currentUser: { name: "Adriana" },
-      messages: [
-        {
-          type: "incomingMessage",
-          content:
-            "I won't be impressed with technology until I can download food.",
-          username: "Adriana"
-        },
-        {
-          type: "incomingNotification",
-          content: "Anonymous1 changed their name to nomnom"
-        },
-        {
-          type: "incomingMessage",
-          content:
-            "I wouldn't want to download Kraft Dinner. I'd be scared of cheese packet loss.",
-          username: "Anonymous2"
-        },
-        {
-          type: "incomingMessage",
-          content: "...",
-          username: "nomnom"
-        },
-        {
-          type: "incomingMessage",
-          content:
-            "I'd love to download a fried egg, but I'm afraid encryption would scramble it",
-          username: "Anonymous2"
-        },
-        {
-          type: "incomingMessage",
-          content: "This isn't funny. You're not funny",
-          username: "nomnom"
-        },
-        {
-          type: "incomingNotification",
-          content: "Anonymous2 changed their name to NotFunny"
-        }
-      ]
+      socket: null,
+      messages: []
     };
   }
   componentDidMount() {
-    console.log("componentDidMount <App />");
-    setTimeout(() => {
-      console.log("Simulating incoming message");
-      // Add a new message to the list of messages in the data store
-      const newMessage = {
-        id: 3,
-        username: "Michelle",
-        content: "Hello there!"
-      };
-      console.log("this is my message", this.state);
-      const messages = this.state.messages.concat(newMessage);
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({ messages: messages });
-    }, 3000);
+    const socket = new WebSocket("ws://localhost:3001");
+    socket.onopen = function(event) {
+      console.warn("socket connection established");
+    };
+
+    this.setState({ socket: socket });
   }
 
-  sendMessage = (message, username) => {
-    console.log("message", message);
-    const oldMessages = this.state.messages;
-    const newChat = {
-      id: generateRandomId,
+  sendMessage = (content, username) => {
+    console.log("content", content);
+    const message = {
+      type: "postMessage",
+      id: generateRandomString(),
       username: username,
-      content: message
+      content: content
     };
-    const newMessages = [...oldMessages, newChat];
-    this.setState({ messages: newMessages });
-    console.log(newChat);
+    this.state.socket.send(JSON.stringify(message));
+    console.log(JSON.stringify(message));
   };
 
   render() {
